ae7f03a9a583a9a0302c9a783811e401eed319b7
chark1es <charles15851@gmail.com>
2023-10-09 14:50:31 -0700

    pages ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,13 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import NetlifyCMS from "astro-decap-cms";
 import mdx from "@astrojs/mdx";
-import { decapCmsOauth } from "astro-decap-cms-oauth";
+import decapCmsOauth from "astro-decap-cms-oauth";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [decapCmsOauth(), tailwind(), mdx()],
-    output: "server"
+    output: "server",
+    adapter: cloudflare(),
+    platform: "node"
 });
