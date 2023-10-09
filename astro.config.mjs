186ef9fb171ed0805e9a122438da07e807af94f7
chark1es <charles15851@gmail.com>
2023-10-08 23:36:07 -0700

    added decap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,30 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
+import NetlifyCMS from "astro-decap-cms";
 import mdx from "@astrojs/mdx";
+import { decapCmsOauth } from "astro-decap-cms-oauth";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()]
+    integrations: [
+        decapCmsOauth(),
+        
+        tailwind(),
+        mdx()
+    ]
+    output: "server",
 });
+
+export interface DecapCMSOptions {
+  adminRoute?: string;
+  oauthDisabled?: boolean;
+  oauthLoginRoute?: string;
+  oauthCallbackRoute?: string;
+}
+
+const defaultOptions: DecapCMSOptions = {
+  adminRoute: "/admin",
+  oauthDisabled: false,
+  oauthLoginRoute: "/oauth",
+  oauthCallbackRoute: "/oauth/callback",
+};
